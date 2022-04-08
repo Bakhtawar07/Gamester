e1b46d916f90b6950ae669278e9b4aec9bd90f5c
Bakhtawar <bakhtawar@as-MacBook-Pro.local>
2022-04-08 16:44:17 +0500

    React Native Concepts Implementation

--- src/Navigators/Application.js
+++ src/Navigators/Application.js
@@ -6,28 +6,52 @@ import { StartupContainer } from '@/Containers'
 import { useTheme } from '@/Hooks'
 import MainNavigator from './Main'
 import { navigationRef } from './utils'
+import LoginScreen from '@/Containers/LoginScreen'
+import { useSelector } from 'react-redux'
 
 const Stack = createStackNavigator()
 
-// @refresh reset
 const ApplicationNavigator = () => {
   const { Layout, darkMode, NavigationTheme } = useTheme()
   const { colors } = NavigationTheme
 
+  const isLogin = useSelector(state => {
+    return state?.userLoginStatus?.value
+  }) 
+  const AppStack = () => (
+    <>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Startup" component={StartupContainer} />
+        <Stack.Screen
+          name="Main"
+          component={MainNavigator}
+          options={{
+
+            animationEnabled: false,
+          }}
+        />
+      </Stack.Navigator>
+    </>
+  )
+
+  const AuthStack = () => (
+    <>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            animationEnabled: false,
+          }}
+        />
+      </Stack.Navigator>
+    </>
+  )
+
   return (
     <SafeAreaView style={[Layout.fill, { backgroundColor: colors.card }]}>
       <NavigationContainer theme={NavigationTheme} ref={navigationRef}>
-        <StatusBar barStyle={darkMode ? 'light-content' : 'dark-content'} />
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Startup" component={StartupContainer} />
-          <Stack.Screen
-            name="Main"
-            component={MainNavigator}
-            options={{
-              animationEnabled: false,
-            }}
-          />
-        </Stack.Navigator>
+          {isLogin ? AppStack() : AuthStack()}
       </NavigationContainer>
     </SafeAreaView>
   )
